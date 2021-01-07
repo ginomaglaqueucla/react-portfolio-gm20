cd7123d6a9ed0450469fd19febe629b3de99a5cb
ginomaglaque <ginomaglaque@gmail.com>
2021-01-06 19:22:02 -0800

    About me section

---
+++ react-portfolio/src/components/About/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import coverImage from "../../assets/cover/cover-image.jpg";
+
+
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about">Who am I?</h1>
+      {/* <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" /> */}
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Nunc ultricie Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Nunc ultricie Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Nunc ultricie
+        </p>
+    </section>
+  );
+}
+
+export default About;
