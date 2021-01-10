a0199abc1503ebbca3e9f7fa86ee40048b9f7e34
ginomaglaque <ginomaglaque@gmail.com>
2021-01-09 16:16:26 -0800

    headshot

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -4,11 +4,16 @@ function About() {
   return (
     <section className="my-5">
       <h1 id="about">Who am I?</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Nunc ultricie Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Nunc ultricie Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Nunc ultricie
-        </p>
+      <div>
+        <img src={require(`../../assets/IMG_4257.JPG`).default} className='gino-img'></img>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Nunc ultricie Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Nunc ultricie Nunc ultricieNunc ultricieNunc ultricieNunc ultricieNunc ultricieNunc ultricie
+            Nunc ultricieNunc ultricieNunc ultricieNunc ultricieNunc ultricieNunc ultricieNunc ultricie
+            Nunc ultricieNunc ultricieNunc ultricieNunc ultricieNunc ultricieNunc ultricie
+          </p>
+      </div>
+
     </section>
   );
 }
--- react-portfolio/src/components/Footer/index.js
+++ react-portfolio/src/components/Footer/index.js
@@ -5,13 +5,13 @@ function Footer() {
     <footer>
         <ul>
             <li>
-                <a>
-                    github
+                <a href='https://drive.google.com/file/d/1Y5RPY1ghopVbxfRkAkqpsLjIzMFF8TRl/view?usp=sharing'>
+                   MY GITHUB
                 </a>
             </li>
             <li>
-                <a>
-                    LinkedIn
+                <a href='https://www.linkedin.com/in/gino-maglaque-16820bb9/'>
+                    My LinkedIn
                 </a>
             </li>
         </ul>
